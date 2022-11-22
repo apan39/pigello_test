51c5ec0f540d239d128283a4302fcfcc07d613fc
johan <gnarp24@gmail.com>
2022-11-22 20:31:57 +0100

    added subcomponent structure for bodies

--- src/Components/SolarSystemBodies.js
+++ src/Components/SolarSystemBodies.js
@@ -1,9 +1,33 @@
-function SolarSystemBodies (){
-    return(
-        <div>
-            <h1>from solar system</h1>
-        </div>
-        )
+function SolarSystemBodies() {
+  return (
+    <div>
+      <h1>from solar system</h1>
+    </div>
+  );
 }
 
 export default SolarSystemBodies;
+
+function BodiesList(props) {
+  return (
+    <>
+      <h1>Bodieslist</h1>
+    </>
+  );
+}
+
+function ListItem(props) {
+  return (
+    <>
+      <h1>listitem</h1>
+    </>
+  );
+}
+
+function ListSubItem(props) {
+  return (
+    <>
+      <h1>listSubitem</h1>
+    </>
+  );
+}
